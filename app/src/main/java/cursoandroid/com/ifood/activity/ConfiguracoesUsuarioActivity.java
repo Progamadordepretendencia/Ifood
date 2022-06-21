2acc912067a2ca29bc3b2417b706f50987e6d48e
Progamadordepretendencia <pirulitodepalmito09@gmail.com>
2022-06-21 15:20:33 -0300

    projeto final de pdm

---
+++ app/src/main/java/cursoandroid/com/ifood/activity/ConfiguracoesUsuarioActivity.java
@@ -0,0 +1,16 @@
+package cursoandroid.com.ifood.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cursoandroid.com.ifood.R;
+
+public class ConfiguracoesUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes_usuario);
+    }
+}
--- app/src/main/java/cursoandroid/com/ifood/activity/HomeActivity.java
+++ app/src/main/java/cursoandroid/com/ifood/activity/HomeActivity.java
@@ -1,14 +1,82 @@
 package cursoandroid.com.ifood.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
 import cursoandroid.com.ifood.R;
+import cursoandroid.com.ifood.helper.ConfiguravaoFirebase;
 
 public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth autenticacao;
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        inicializarComponentes();
+        autenticacao = ConfiguravaoFirebase.getFirebaseAutenticacao();
+
+        //Configurações Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Ifood");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_usuario, menu);
+
+        //configuração botão de pesquisa
+        MenuItem item = menu.findItem(R.id.menupesquisa);
+        searchView.setMenuItem(item);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.sair:
+                deslogarUsuario();
+                break;
+            case R.id.menuConfiguracoes :
+                abrirConfiguracoes();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void inicializarComponentes(){
+        searchView = findViewById(R.id.materialSearchView);
+    }
+
+    private void deslogarUsuario(){
+        try {
+            autenticacao.signOut();
+            finish();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void abrirConfiguracoes(){
+        startActivity(new Intent(HomeActivity.this, ConfiguracoesUsuarioActivity.class));
     }
 }
